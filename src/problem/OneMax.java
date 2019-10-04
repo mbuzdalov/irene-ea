4c739cbaee888d3eba9ad88cd6110f4718317e71
kant97 <kirant9797@gmail.com>
2019-10-04 16:28:10 +0300

    refactor - extracted duplicated code to base class

--- src/problem/OneMax.java
+++ src/problem/OneMax.java
@@ -1,58 +1,34 @@
 package problem;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
-public class OneMax implements Problem {
-    private boolean[] individual;
-    private int fitness;
+public class OneMax extends AbstractProblem {
 
     public OneMax(int n) {
-        individual = new boolean[n];
-        Random rand = new Random();
-        fitness = 0;
-        for (int i = 0; i < n; ++i) {
-            individual[i] = rand.nextBoolean();
-            if (individual[i]) {
-                fitness++;
+        super(n);
+    }
+
+    @Override
+    protected int getHonestFitness() {
+        int fit = 0;
+        for (boolean b : myOffspring) {
+            if (b) {
+                fit++;
             }
         }
+        return fit;
     }
 
     @Override
     public int calculatePatchFitness(List<Integer> patch) {
-        int newFitness = fitness;
+        int newFitness = myFitness;
         for (Integer i : patch) {
-            if (individual[i]) {
+            if (myOffspring[i]) {
                 newFitness--;
             } else {
                 newFitness++;
             }
         }
         return newFitness;
-    }
-
-    @Override
-    public void applyPatch(List<Integer> patch, int fitness) {
-        for (Integer i : patch) {
-            individual[i] = !individual[i];
-        }
-        this.fitness = fitness;
-    }
-
-    @Override
-    public int getFitness() {
-        return fitness;
-    }
-
-    @Override
-    public int getLength() {
-        return individual.length;
-    }
-
-    @Override
-    public boolean isOptimized() {
-        return fitness == individual.length;
     }
 }
